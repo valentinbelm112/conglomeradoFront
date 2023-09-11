5227b8c0c8cf4d4dd8ce358c83afbc52c8530081
Valentin Antunez <vantunez@proempresa.com.pe>
2023-09-11 08:55:36 -0500

    Add upload view propietario

--- src/hooks/usePostDocumentProp.js
+++ src/hooks/usePostDocumentProp.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
-import { accesUrlTocken } from "../utils/Configuration";
+import { serverURL } from '../utils/Configuration';
 export const refreshAccessToken = async (refreshToken) => {
     try {
-      const response = await axios.post(refreshToken, {
-        grant_type: 'refresh_token',
-        client_id: 'refresh_token',
-        client_secret: 'refresh_token',
-        refresh_token: 'refresh_token',
+      
+      const response = await axios.get(`${API}?Codigo_Asociacion=${codigo_asociacion}`); 
 
-      });
-  
+      
+      //console.log(response);
+      //console.log(response);
       return response.data.access_token;
+
     } catch (error) {
       console.error('Error refreshing access token:', error);
       throw error;
